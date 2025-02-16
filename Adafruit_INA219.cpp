dff7f096f3069e0ec176d9adf20dd113561f4832
Dylan Bruschi <dylanbruschi54@gmail.com>
2025-02-16 01:38:25 -0500

    Update gain for current sensor

--- Adafruit_INA219.cpp
+++ Adafruit_INA219.cpp
@@ -553,7 +553,7 @@ void Adafruit_INA219::setCalibration_Sunkara() {
   calibration_reg.write(ina219_calValue, 2);
   // Set Config register to take into account the settings above
   uint16_t config = INA219_CONFIG_BVOLTAGERANGE_32V |
-    INA219_CONFIG_GAIN_8_320MV | INA219_CONFIG_BADCRES_12BIT |
+    INA219_CONFIG_GAIN_1_40MV | INA219_CONFIG_BADCRES_12BIT |
                     INA219_CONFIG_SADCRES_12BIT_1S_532US |
                     INA219_CONFIG_MODE_SANDBVOLT_CONTINUOUS;
 
